d02c24cf8a17f5118ae7c0c9283d650fb928ce47
fahreyad <fahreyadome@gmail.com>
2026-09-16 12:50:33 +0600

    add server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Go api now running fast 12312312...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+	serv := http.Server{
+		Addr:         ":8999",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	if err := serv.ListenAndServe(); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
