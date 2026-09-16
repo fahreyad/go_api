b7f34a9163ac47a6eed5bf2114ea806fd4240f5e
fahreyad <fahreyadome@gmail.com>
2026-09-16 16:54:15 +0600

    env change

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,9 +13,8 @@ type Config struct {
 }
 
 func MustLoad() Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found, using environment variables")
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
